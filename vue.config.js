12dce3698f54b710bcd1a1c3aeb40fecf0aa3202
Jonatha Daguerre Vasconcelos <jonatha@daguerre.com.br>
2020-04-18 16:35:15 -0300

    adds build date

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
+process.env.VUE_APP_BUILD_DATE = String(new Date())
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/my-name/" : "/"
+  publicPath: process.env.NODE_ENV === "production" ? "/my-name/" : "/",
+  
 };
