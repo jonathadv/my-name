25bb8ef1be3db7d20dff6dafe3c7cf9d3388059a
Jonatha Daguerre Vasconcelos <jonatha@daguerre.com.br>
2024-08-05 18:42:31 -0300

    New version

---
+++ precache-manifest.2b8a9d17d578bc429d74d07757730e8c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6cbfbac00e4daeeccee",
+    "url": "/my-name/css/app.70bf384f.css"
+  },
+  {
+    "revision": "c8cf8bd8b98f936a589bd23b87d253f4",
+    "url": "/my-name/img/logo.c8cf8bd8.jpg"
+  },
+  {
+    "revision": "d00146d47e76a833cf5fe635d7538cf9",
+    "url": "/my-name/index.html"
+  },
+  {
+    "revision": "c6cbfbac00e4daeeccee",
+    "url": "/my-name/js/app.08ec6c85.js"
+  },
+  {
+    "revision": "b0ec5ffaeb2dafb17b90",
+    "url": "/my-name/js/chunk-vendors.201fd4d4.js"
+  },
+  {
+    "revision": "43ef3b4a0ed00a60d9826cbb1b277311",
+    "url": "/my-name/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/my-name/robots.txt"
+  }
+]);
